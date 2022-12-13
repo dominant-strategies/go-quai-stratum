27dcf9412861cb1f55f23ba4933d32d507f6c6ba
CircuitBreaker <circuitbreaker@pop-os.localdomain>
2022-12-13 12:34:32 -0500

    Revert "Add NiceHash Support"
    This reverts commit 410a6b713a6d4564588047c097078e8529455801.

--- proxy/config.go
+++ proxy/config.go
@@ -33,16 +33,15 @@ type Config struct {
 }
 
 type Proxy struct {
-	Enabled              bool    `json:"enabled"`
-	Listen               string  `json:"listen"`
-	LimitHeadersSize     int     `json:"limitHeadersSize"`
-	LimitBodySize        int64   `json:"limitBodySize"`
-	BehindReverseProxy   bool    `json:"behindReverseProxy"`
-	BlockRefreshInterval string  `json:"blockRefreshInterval"`
-	Difficulty           int64   `json:"difficulty"`
-	DifficultyNiceHash   float64 `json:"difficultyNiceHash"`
-	StateUpdateInterval  string  `json:"stateUpdateInterval"`
-	HashrateExpiration   string  `json:"hashrateExpiration"`
+	Enabled              bool   `json:"enabled"`
+	Listen               string `json:"listen"`
+	LimitHeadersSize     int    `json:"limitHeadersSize"`
+	LimitBodySize        int64  `json:"limitBodySize"`
+	BehindReverseProxy   bool   `json:"behindReverseProxy"`
+	BlockRefreshInterval string `json:"blockRefreshInterval"`
+	Difficulty           int64  `json:"difficulty"`
+	StateUpdateInterval  string `json:"stateUpdateInterval"`
+	HashrateExpiration   string `json:"hashrateExpiration"`
 
 	Policy policy.Config `json:"policy"`
 
