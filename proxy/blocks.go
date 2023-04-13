7e29696d0477c5bfc0c8acff5c6ae52d4a3c3d0d
Djadih <13135116+Djadih@users.noreply.github.com>
2023-04-13 13:25:09 -0500

    Improved broadcast efficiency, no longer push duplicate headers

--- proxy/blocks.go
+++ proxy/blocks.go
@@ -1,19 +1,17 @@
 package proxy
 
 import (
-	"errors"
 	"log"
 	"math/big"
 
 	"sync"
 
 	"github.com/dominant-strategies/go-quai/common"
+	"github.com/dominant-strategies/go-quai/consensus/blake3pow"
 	"github.com/dominant-strategies/go-quai/core/types"
 
 	"github.com/dominant-strategies/go-quai/common/hexutil"
 )
-
-const maxBacklog = 3
 
 type BlockTemplate struct {
 	sync.RWMutex
@@ -21,7 +19,6 @@ type BlockTemplate struct {
 	Target     *big.Int
 	Difficulty *big.Int
 	Height     []*big.Int
-	nonces     map[string]bool
 }
 
 type Block struct {
@@ -44,18 +41,18 @@ func (s *ProxyServer) fetchBlockTemplate() {
 		log.Printf("Error while getting pending header (work) on %s: %s", rpc.Name, err)
 		return
 	}
-	// No need to update, we have fresh job
-	if t != nil && t.Header == pendingHeader {
+
+	// Short circuit if the pending header is the same as the current one
+	if t != nil && t.Header != nil && t.Header.SealHash() == pendingHeader.SealHash() {
 		return
 	} else if t != nil {
 		t.Header = pendingHeader
 	}
 
 	newTemplate := BlockTemplate{
-		Header:     pendingHeader,
-		Target:     pendingHeader.DifficultyArray()[common.ZONE_CTX],
-		Height:     pendingHeader.NumberArray(),
-		Difficulty: pendingHeader.DifficultyArray()[common.ZONE_CTX],
+		Header: pendingHeader,
+		Target: blake3pow.DifficultyToTarget(pendingHeader.Difficulty()),
+		Height: pendingHeader.NumberArray(),
 	}
 
 	s.blockTemplate.Store(&newTemplate)
@@ -64,25 +61,4 @@ func (s *ProxyServer) fetchBlockTemplate() {
 	if s.config.Proxy.Stratum.Enabled {
 		go s.broadcastNewJobs()
 	}
-}
-
-var (
-	big2e256 = new(big.Int).Exp(big.NewInt(2), big.NewInt(256), big.NewInt(0)) // 2^256
-)
-
-// This function determines the difficulty order of a block
-func GetDifficultyOrder(header *types.Header) (int, error) {
-	if header == nil {
-		return common.HierarchyDepth, errors.New("no header provided")
-	}
-	blockhash := header.Hash()
-	for i, difficulty := range header.DifficultyArray() {
-		if difficulty != nil && big.NewInt(0).Cmp(difficulty) < 0 {
-			target := new(big.Int).Div(big2e256, difficulty)
-			if new(big.Int).SetBytes(blockhash.Bytes()).Cmp(target) <= 0 {
-				return i, nil
-			}
-		}
-	}
-	return -1, errors.New("block does not satisfy minimum difficulty")
 }
